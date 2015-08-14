d74e5422f7d07d894fc67defb5914e1b1672179e
Ryan Bluth <ryanbluth93@gmail.com>
2015-08-13 22:45:40 -0400

    Pulling over ians changes

--- static/js/scenarioEditor/charView/charView.js
+++ static/js/scenarioEditor/charView/charView.js
@@ -9,10 +9,34 @@ angular.module('scenarioEditor.charView', ['ngRoute'])
   });
 }])
 
+.service('convoService', function () {
+    var convoData = [
+        {'id':0,'name':'Conversation 0'}
+    ];
+
+    var currConversation = 0;
+
+    return {
+        conversations:function () {
+            return convoData;
+        },
+        addConversation:function () {
+            currConversation++;
+            convoData.push({'id':currConversation,'name':'Conversation '+currConversation});
+        },
+        editConversation:function (convo) {
+            //TODO: Make this work
+        },
+        deleteConversation:function (convo) {
+          convoData.splice(convoData.indexOf(convo),1);
+        }
+    };
+})
+
 .service('charService', function () {
-	var charData = [
-		{'id':0,'name':'','other':''}
-	];
+	var charData = [];
+
+	var charId = 0;
 
 	var currChar = 0;
 
@@ -21,16 +45,35 @@ angular.module('scenarioEditor.charView', ['ngRoute'])
 			return charData;
 		},
 		addChar:function () {
-			currChar++;
-			charData.push({'id':currChar,'name':'',other:''});
+			charId++;
+			charData.push({'id':charId,'name':'','states':[]});
 		},
 		deleteChar:function (character) {
 			charData.splice(charData.indexOf(character),1);
+		},
+		editChar:function (character) {
+			currChar = character.id;
+		},
+		getCurrChar:function () {
+			return currChar;
+		},
+		addStateToChar:function (character,id) {
+			charData[charData.indexOf(character)].states.push({'id':id,'name':'','convoId':0});
+		},
+		getStatesLength:function (character) {
+			return charData[charData.indexOf(character)].states.length;
 		}
 	};
 })
 
-.controller('CharCtrl', ['$scope', 'charService', function($scope, charService) {
+.controller('CharCtrl', ['$scope', 'charService', 'convoService', function($scope, charService, convoService) {
+	$scope.editVisible = false;
+
+	$scope.stateId = 0;
+
+	$scope.currBodyPart = "";
+	$scope.editBodyPartVisible = false;
+
 	$scope.getChars = function () {
 		return charService.chars();
 	};
@@ -39,8 +82,45 @@ angular.module('scenarioEditor.charView', ['ngRoute'])
 		return charService.addChar();
 	};
 
-	$scope.deleteChar = function (id) {
-		return charService.deleteChar(id);
+	$scope.deleteChar = function (chara) {
+		return charService.deleteChar(chara);
 	};
+
+	$scope.editChar = function (chara) {
+		$scope.editVisible = true;
+		$scope.stateId = charService.getStatesLength(chara);
+		return charService.editChar(chara);
+	};
+
+	$scope.getCurrChar = function () {
+		return charService.getCurrChar();
+	};
+
+	$scope.getConvos = function () {
+		return convoService.conversations();
+	};
+
+	$scope.getStates = function (character) {
+		return character.states;
+	};
+
+	$scope.addState = function (character) {
+		$scope.stateId++;
+		return charService.addStateToChar(character,$scope.stateId);
+	};
+
+	$scope.deleteState = function (character,state) {
+		character.states.splice(character.states.indexOf(state),1);
+	};
+
+	$scope.editBodyPart = function (name) {
+		$scope.editBodyPartVisible = true;
+		$scope.currBodyPart = name;
+	}
+
+	$scope.closeBodyPart = function () {
+		$scope.editBodyPartVisible = false;
+	}
+
 
 }]);
