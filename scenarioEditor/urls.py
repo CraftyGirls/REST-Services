95f6aeccd3694ddf067fc8a58cf1cb466fc616ea
Ryan Bluth <ryanbluth93@gmail.com>
2015-08-13 21:51:30 -0400

    Added login and register

--- scenarioEditor/urls.py
+++ scenarioEditor/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import url
+
 import scenarioEditor.views
 
 urlpatterns = patterns('',
-                       (r'^$', scenarioEditor.views.index),
-                       (r'^charView/', scenarioEditor.views.charView),
-                       (r'^convoView/', scenarioEditor.views.convoView)
+                       url(r'^$', scenarioEditor.views.index, name='index'),
+                       url(r'^charView/', scenarioEditor.views.charView, name='charView'),
+                       url(r'^convoView/', scenarioEditor.views.convoView, name='convoView'),
+                       url(r'^login/', scenarioEditor.views.login_view, name='login_view'),
+                       url(r'^register/', scenarioEditor.views.register_view, name='register_view')
                        )
--- scenarioEditor/views.py
+++ scenarioEditor/views.py
@@ -1,13 +1,52 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect, render_to_response
+from django.contrib.auth import authenticate, login
+from django.template import Template, Context, RequestContext
+from django.contrib.auth.models import User
+from django.template.loader import get_template
+from api.models import PDUser
 
 
 def index(request):
-    return render(request, 'scenarioEditor/index.html', {})
+    return render_to_response('scenarioEditor/index.html/', RequestContext(request, {}))
 
 
 def charView(request):
-    return render(request, 'scenarioEditor/charView/charView.html', {})
+    return render(request, 'scenarioEditor/charView/charView.html/', {})
 
 
 def convoView(request):
-    return render(request, 'scenarioEditor/convoView/convoView.html', {})
+    return render(request, 'scenarioEditor/convoView/convoView.html/', {})
+
+
+def login_view(request):
+    if 'username' in request.POST and 'password' in request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('index')
+            else:
+                # Return a 'disabled account' error message
+                return render(request, 'scenarioEditor/login.html/', {'login_error': 2})
+        else:
+            # Return an 'invalid login' error message.
+            return render(request, 'scenarioEditor/login.html/', {'login_error': 1})
+    else:
+        return render(request, 'scenarioEditor/login.html/', {'login_error': 0})
+
+
+def register_view(request):
+    if 'username' in request.POST and 'password' in request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('index')
+    else:
+        return render(request, 'scenarioEditor/register.html/', {})
