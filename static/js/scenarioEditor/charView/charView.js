f9bd5f80354b5770e8c5f3cb7a56144e3f8f2897
ryanbluth <ryanbluth93@outlook.com>
2015-10-20 16:41:35 +0000

    Moved file upload directive to asset view

--- static/js/scenarioEditor/charView/charView.js
+++ static/js/scenarioEditor/charView/charView.js
@@ -65,31 +65,5 @@ angular.module('scenarioEditor.charView', ['ngRoute', 'scenarioServices'])
             $scope.editBodyPartVisible = false;
         };
     }])
-    // Directive for dropzone file uploader
-    .directive('fileUploader', function () {
-        return {
-            restrict: 'AE',
-            template: '<div ng-transclude></div>',
-            transclude: true,
-            scope: {
-                dropzone: '=',
-                dropzoneConfig: '=',
-                eventHandlers: '='
-            },
-            link: function(scope, element, attrs, ctrls) {
-                try { Dropzone }
-                catch (error) {
-                    throw new Error('Dropzone.js not loaded.');
-                }
-                var dropzone = new Dropzone(element[0], { url: "/scenario/upload_asset/"});
-
-                if (scope.eventHandlers) {
-                    Object.keys(scope.eventHandlers).forEach(function (eventName) {
-                        dropzone.on(eventName, scope.eventHandlers[eventName]);
-                    });
-                }
-                scope.dropzone = dropzone;
-            }
-        };
-    });
+    
 
