5c1b106bc1be69ebfb3360f43bde9e90da2dd8d3
Ryan Bluth <ryanbluth93@gmail.com>
2016-02-28 14:24:57 -0500

    Move stengine page to site

--- sweetheartsquad/views.py
+++ sweetheartsquad/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'sweetheartsquad/index.html', {})
+
+
+def stengine(request):
+    return render(request, 'sweetheartsquad/stengine2.html', {})
